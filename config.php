fa149e49b2914c4a950169b7d3f267c67824956b
Deep Rank <57340777+rankdeep@users.noreply.github.com>
2022-08-22 17:46:45 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'test';
+
+$action = array('update','delete');
+$conn = mysqli_connect($hostname, $username, $password, $dbname); 
+ 
+?>
